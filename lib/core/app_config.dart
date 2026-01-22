7aee13b8691a1e8b41e97209395a675bcbdea137
Thịnh <phungthinh789@gmail.com>
2026-01-22 13:09:47 +0700

    Switch to cloud backend API

--- lib/core/app_config.dart
+++ lib/core/app_config.dart
@@ -2,14 +2,14 @@
 
 class AppConfig {
   static String get baseUrl {
-    // Local Docker Backend
-    // Nếu test trên điện thoại thật: dùng IP máy tính (192.168.1.220)
+    // API Render Cloud
+    return "https://backend-led-xaxn.onrender.com/api";
+    
+    // Local Docker Backend (uncomment nếu muốn dùng local)
+    // Nếu test trên điện thoại thật: dùng IP máy tính (192.168.1.28)
     // Nếu test trên Android Emulator: dùng 10.0.2.2
     // Nếu test trên iOS Simulator: dùng localhost
-    return "http://192.168.1.220:8080/api";
-    
-    // API Render Cloud (backup)
-    // return "https://backend-led-xaxn.onrender.com/api";
+    // return "http://192.168.1.28:8080/api";
   }
   static String get mqttBroker {
     // HiveMQ Cloud Broker
